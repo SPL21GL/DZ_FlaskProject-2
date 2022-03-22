0185a9f26d6cd182e59cd182f8f338731434d941
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-03-22 10:21:53 +0100

    added CSRF

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask.templating import render_template
+from flask_wtf.csrf import CSRFProtect
+from model.models import db
 
+import sqlalchemy
 app = Flask(__name__)
+app.secret_key = "VerySecretSecretKey"
 
-@app.route("/")
-def index():
-    return "<h1>Hallo</h1>"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/classicModels"
 
-app.run()
+csrf = CSRFProtect(app)
+
+db.init_app(app)
+
+#hier blueprint registrieren
+
+
+app.run(debug=True)
