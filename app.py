020dd705467e739c89f418dc17a80a3040e16156
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-04-05 10:17:04 +0200

    added addKundenForm

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask.templating import render_template
 from flask_wtf.csrf import CSRFProtect
-from model.models import db
-import sqlalchemy
+from addKundenForm import AddKundenForm
+from model.models import Fahrrad, Kunden, db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", headline = "Fahrrad-App")
+    addKundenForm = AddKundenForm()
+    
+    if addKundenForm.validate_on_submit():
+        print(addKundenForm.VorName.data)
+        print(addKundenForm.NachName.data)
+        print(addKundenForm.Geburtsdatum.data)
+        print(addKundenForm.Email.data)
+        
+        newKunde = Kunden()
+        newKunde.title = addKundenForm.VorName.data
+        newKunde.description = addKundenForm.NachName.data
+        newKunde.dueDate = addKundenForm.Geburtsdatum.data
+        newKunde.isDone = addKundenForm.Email.data
 
-app.secret_key = "VerySecretSecretKey"
+        db.session.add(newKunde)
+        db.session.commit()
+
+        return redirect("/")
+        
+    
+    items = db.session.query(Kunden).all()
+
+    return render_template("index.html", \
+        headline="Todo Items", \
+        form = addKundenForm, \
+        items = items)
+
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:root@localhost/fahrradApp"
