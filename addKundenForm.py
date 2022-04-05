dce10d7186cc22be45135ba5427ed514c0bce046
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-04-05 10:17:37 +0200

    added addKundenForm.py

---
+++ addKundenForm.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.datetime import DateField
+from wtforms.fields.simple import StringField, TextAreaField
+from wtforms import validators
+
+class AddKundenForm(FlaskForm):
+    VorName = StringField("VorName",  validators=[validators.InputRequired()])
+    NachName = StringField("NachName",  validators=[validators.InputRequired()])
+    Geburtsdatum = DateField("Geburtsdatum")
+    Email = TextAreaField("Email")
