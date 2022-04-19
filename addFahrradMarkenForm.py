c31b96761ed45ec5df0ba91d5ce5a50d638230cf
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-04-19 09:57:50 +0200

    added addFahrradMarkenForm.py

---
+++ addFahrradMarkenForm.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms.fields.datetime import DateField
+from wtforms.fields.simple import StringField, TextAreaField
+from wtforms import validators
+
+class AddFahrradMarkenForm(FlaskForm):
+    FahrradID = StringField("FahrradID",  validators=[validators.InputRequired()])
+    MarkenName = StringField("MarkenName",  validators=[validators.InputRequired()])
+    CEO = StringField("CEO", validators=[validators.InputRequired()])
+    Email = StringField("Email",  validators=[validators.InputRequired()])
+    Standort = StringField("Standort",  validators=[validators.InputRequired()])
