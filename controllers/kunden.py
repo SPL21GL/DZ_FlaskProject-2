9a796afa3b6ec8127ccffd9e99e2b98629240ceb
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-06-18 01:03:14 +0200

    added delete_Kunden

--- controllers/kunden.py
+++ controllers/kunden.py
@@ -1,8 +1,10 @@
-from flask import Flask, redirect, request, flash, session
+from flask import redirect, flash, request
 from flask.templating import render_template
 from flask import Blueprint
 from forms.addKundenForm import AddKundenForm
 from model.models import db, Kunden
+from forms.delete_Kunden import DeleteForm_Kunden
+from forms.edit_Kunden import EditKundenForm
 
 kunden_blueprint = Blueprint('kunden_blueprint', __name__)
 
@@ -32,3 +34,66 @@ def kunden_base():
     return render_template("kunden/kunden.html",
                            form=addKundenForm,
                            kunden=kunden)
+
+
+@kunden_blueprint.route("/kunden/delete", methods=["post"])
+def del_kunden_base():
+    delete_Kunden = DeleteForm_Kunden()
+    if delete_Kunden.validate_on_submit():
+        print("Gültig")
+
+        Kunde_to_delete = delete_Kunden.KundenID.data
+        Kunde_to_delete = db.session.query(Kunden).filter(
+            Kunden.KundenID == Kunde_to_delete)
+        Kunde_to_delete.delete()
+
+        db.session.commit()
+
+    else:
+        print("Fatal Error")
+    flash(f"Kunde with id {Kunde_to_delete} has been deleted")
+    return redirect("/kunden")
+
+
+@kunden_blueprint.route("/kunden/edit", methods=["post"])
+def edit_kunden_base():
+    edit_Kunden = EditKundenForm()
+
+    if edit_Kunden.validate_on_submit():
+        print("Submit wurde durchgeführt")
+
+        KundenID = edit_Kunden.KundenID.data
+        Kunden_to_edit = db.session.query(Kunden).filter(
+            Kunden.KundenID == KundenID).first()
+
+        Kunden_to_edit.VorName = edit_Kunden.VorName.data
+        Kunden_to_edit.NachName = edit_Kunden.NachName.data
+        Kunden_to_edit.Geburtsdatum = edit_Kunden.Geburtsdatum.data
+        Kunden_to_edit.Email = edit_Kunden.Email.data
+
+        db.session.commit()
+
+        return redirect("/kunden")
+
+    else:
+        raise ("Fatal Error")
+
+
+@kunden_blueprint.route("/edit_kunden")
+def showEditKundenForm():
+
+    KundenID = request.args["KundenID"]
+    print(KundenID)
+
+    Kunde_to_edit = db.session.query(Kunden).filter(
+        Kunden.KundenID == KundenID).first()
+
+    edit_Kunden = EditKundenForm()
+
+    edit_Kunden.KundenID.data = Kunde_to_edit.KundenID
+    edit_Kunden.VorName.data = Kunde_to_edit.VorName
+    edit_Kunden.NachName.data = Kunde_to_edit.NachName
+    edit_Kunden.Geburtsdatum.data = Kunde_to_edit.Geburtsdatum
+    edit_Kunden.Email.data = Kunde_to_edit.Email
+
+    return render_template("kunden/edit_kunden.html", form=edit_Kunden)
