7b2cd5a7bac93f05c82b556ccef0ba82cf061f5a
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-06-18 01:02:05 +0200

    added delete_Ausleihen

--- controllers/ausleihen.py
+++ controllers/ausleihen.py
@@ -1,6 +1,7 @@
 from flask.templating import render_template
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, flash
 from forms.addAusleihenForm import AddAusleihenForm
+from forms.delete_ausleihen import DeleteForm_Ausleihen
 from model.models import db, Ausleihen
 
 ausleihen_blueprint = Blueprint('ausleihen_blueprint', __name__)
@@ -27,3 +28,21 @@ def ausleihen_base():
     return render_template("ausleihen/ausleihen.html",
                            form=addAusleihenForm,
                            ausleihen=ausleihen)
+
+@ausleihen_blueprint.route("/ausleihen/delete", methods=["post"])
+def del_ausleihen_base():
+    delete_Ausleihen = DeleteForm_Ausleihen()
+    if delete_Ausleihen.validate_on_submit():
+        print("Gültig")
+
+        Ausleihen_to_delete = delete_Ausleihen.AusleihenID.data
+        Ausleihen_to_delete = db.session.query(Ausleihen).filter(
+            Ausleihen.AusleihenID == Ausleihen_to_delete)
+        Ausleihen_to_delete.delete()
+
+        db.session.commit()
+
+    else:
+        print("Fatal Error")
+    flash(f"Ausleihen with id {Ausleihen_to_delete} has been deleted")
+    return redirect("/ausleihen")
