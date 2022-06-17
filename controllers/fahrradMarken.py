202e481e2b8e7938ac3751a8b8f0a59eec34dd4c
Zeynep Deveci <zeynep.deveci@bulme.at>
2022-06-18 01:02:50 +0200

    added delete_FahrradMarke

--- controllers/fahrradMarken.py
+++ controllers/fahrradMarken.py
@@ -1,7 +1,8 @@
 from flask.templating import render_template
-from flask import Blueprint, redirect
+from flask import Blueprint, redirect, flash
 from forms.addFahrradMarkenForm import AddFahrradMarkenForm
 from model.models import db, Fahrradmarke
+from forms.delete_FahrradMarken import DeleteForm_FahrradMarken
 
 fahrradMarken_blueprint = Blueprint('fahrradMarken_blueprint', __name__)
 
@@ -31,3 +32,21 @@ def fahrradMarke_base():
     return render_template("fahrradMarke/fahrradMarken.html",
                            form=addFahrradMarkenForm,
                            fahrradMarke=fahrradMarke)
+
+@fahrradMarken_blueprint.route("/fahrradMarken/delete", methods=["post"])
+def del_fahrradMarken_base():
+    delete_FahrradMarke = DeleteForm_FahrradMarken()
+    if delete_FahrradMarke.validate_on_submit():
+        print("Gültig")
+
+        FahrradMarke_to_delete = delete_FahrradMarke.FahrradMarkenID.data
+        FahrradMarke_to_delete = db.session.query(Fahrradmarke).filter(
+            Fahrradmarke.FahrradMarkenID == FahrradMarke_to_delete)
+        FahrradMarke_to_delete.delete()
+
+        db.session.commit()
+
+    else:
+        print("Fatal Error")
+    flash(f"FahrradMarke with id {FahrradMarke_to_delete} has been deleted")
+    return redirect("/fahrradMarken")
